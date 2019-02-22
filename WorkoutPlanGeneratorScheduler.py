a617e4b1a3b5e98c985efae6c245a65a7249d7e0
Mike Simms <msimms01@gmail.com>
2019-02-21 23:05:38 -0500

    Removed dead code.

--- WorkoutPlanGeneratorScheduler.py
+++ WorkoutPlanGeneratorScheduler.py
@@ -9,14 +9,10 @@ class WorkoutPlanGeneratorScheduler(object):
     """Class for scheduling computationally expensive workout plan generation tasks."""
 
     def __init__(self):
-        self.enabled = True
         super(WorkoutPlanGeneratorScheduler, self).__init__()
 
     def add_to_queue(self, user_id):
         """Adds the user to the list of workout plans to be generated."""
-        if not self.enabled:
-            return
-
         user_obj = {}
         user_obj[Keys.WORKOUT_PLAN_USER_ID] = user_id
         generate_workout_plan.delay(dumps(user_obj))
