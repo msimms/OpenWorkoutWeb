e37a914ac7b8fad272bb1c58b699a856ff586271
Mike Simms <msimms01@gmail.com>
2019-05-23 14:45:45 -0400

    Fixed tab style.

--- VO2MaxCalculator.py
+++ VO2MaxCalculator.py
@@ -12,4 +12,4 @@ class VO2MaxCalculator(object):
 
     def estimate_vo2max_from_race_distance_in_meters(self, race_distance_meters, race_time_minutes):
         speed = race_distance_meters / race_time_minutes
-    	return -4.60 + 0.182258 * speed + 0.000104 * speed * speed
+        return -4.60 + 0.182258 * speed + 0.000104 * speed * speed
