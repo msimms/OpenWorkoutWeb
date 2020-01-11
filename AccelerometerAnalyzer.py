1d4e297ed7bdf5ccc0b67e4d2d7303aa91320903
Mike Simms <msimms01@gmail.com>
2020-01-10 21:50:33 -0500

    Added k-means to the accelerometer analysis.

--- AccelerometerAnalyzer.py
+++ AccelerometerAnalyzer.py
@@ -12,7 +12,9 @@ import SensorAnalyzer
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 libmathdir = os.path.join(currentdir, 'LibMath', 'python')
 sys.path.insert(0, libmathdir)
+import kmeans
 import peaks
+import signals
 
 class AccelerometerAnalyzer(SensorAnalyzer.SensorAnalyzer):
     """Class for performing calculations on accelerometer data."""
@@ -40,9 +42,26 @@ class AccelerometerAnalyzer(SensorAnalyzer.SensorAnalyzer):
     def analyze(self):
         """Called when all sensor readings have been processed."""
         results = SensorAnalyzer.SensorAnalyzer.analyze(self)
-        peak_list = peaks.find_peaks_in_numeric_array(self.x, 1.0)
 
+        # Smooth the speed graph to take out some of the GPS jitter.
+        smoothed_graph = signals.smooth(self.x, 4)
+
+        # Find statistically significant peaks.
+        areas = []
+        peak_list = peaks.find_peaks_in_numeric_array(smoothed_graph, 0.25)
+        for peak in peak_list:
+            areas.append(peak.area)
+
+        # Do a k-means analysis on the computed peaks so we can get rid of any outliers.
+        significant_peaks = []
+        tags = kmeans.kmeans_equally_space_centroids_1_d(areas, 2, 1, len(areas))
+        peak_index = 0
+        for tag in tags:
+            if tag == 1:
+                significant_peaks.append(peak_list[peak_index])
+            peak_index = peak_index + 1
+            
         sets = []
-        sets.append(len(peak_list))
+        sets.append(len(significant_peaks))
         results[Keys.APP_SETS_KEY] = sets
         return results
