a64312f2035db87c5628f35da1f2eb8d4f51bc5e
Mike Simms <msimms01@gmail.com>
2020-02-14 22:20:59 -0500

    Fix typo.

--- Keys.py
+++ Keys.py
@@ -83,6 +83,7 @@ WORKOUT_TYPE_SPEED_RUN = "Speed Run"
 WORKOUT_TYPE_INTERVAL_SESSION = "Interval Session"
 WORKOUT_TYPE_TEMPO_RUN = "Tempo Run"
 WORKOUT_TYPE_EASY_RUN = "Easy Run"
+WORKOUT_TYPE_HILL_REPEATS = "Hill Repeats" # 4-10 repeats, depending on skill level, done at 5K pace
 WORKOUT_TYPE_MIDDLE_DISTANCE_RUN = "Middle Distance Run" # 2 hour run for advanced distance runners
 WORKOUT_TYPE_LONG_RUN = "Long Run"
 WORKOUT_TYPE_OPEN_WATER_SWIM = "Open Water Swim"
@@ -190,7 +191,7 @@ TOTAL_DISTANCE = "Total Distance"
 LONGEST_DISTANCE = "Longest Distance"
 
 # API-only keys.
-SECONDS = "Seconds"
+SECONDS = "seconds"
 
 # Running paces.
 LONG_RUN_PACE = "Long Run Pace"
