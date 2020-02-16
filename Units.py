f8b979a0305e1f942713c90c13fa209c2ea5ec69
Mike Simms <msimms01@gmail.com>
2020-02-16 16:35:03 -0500

    Make it clear the division is not supposed to be over the entire tuple.

--- Units.py
+++ Units.py
@@ -227,7 +227,9 @@ def convert_to_preferred_pace_units(user_mgr, user_id, value, in_distance_units,
     speed = convert_speed(value, in_distance_units, in_time_units, out_distance_units, out_time_units)
     if speed < 0.001:
         return 0.0
-    return 1.0 / speed, out_distance_units, out_time_units
+
+    pace = 1.0 / speed
+    return pace, out_distance_units, out_time_units
 
 def meters_per_sec_to_minutes_per_mile(value):
     speed = convert_speed(value, UNITS_DISTANCE_METERS, UNITS_TIME_SECONDS, UNITS_DISTANCE_MILES, UNITS_TIME_MINUTES)
