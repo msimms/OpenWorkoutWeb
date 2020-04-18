9a9c86401b6a14b9c5f2b9419bbdcf13bec68c59
Mike Simms <msimms01@gmail.com>
2020-04-18 15:54:45 -0400

    Added MIT licenses and updated some module comments.

--- UserMgr.py
+++ UserMgr.py
@@ -21,7 +21,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-"""Managing user accounts"""
+"""Manages user accounts"""
 
 import bcrypt
 import time
