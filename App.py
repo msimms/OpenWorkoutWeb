45ab6b99e9f0a932a36abdab47a6a1558086fa44
Mike Simms <msimms01@gmail.com>
2020-04-30 17:01:58 -0400

    Delete some redundant code.

--- App.py
+++ App.py
@@ -1260,22 +1260,7 @@ class App(object):
     @statistics
     def task_status(self):
         """Renders the status page for deferred tasks, such as file imports and activity analysis."""
-
-        # Get the logged in user.
-        username = self.user_mgr.get_logged_in_user()
-        if username is None:
-            raise RedirectException(LOGIN_URL)
-
-        # Get the details of the logged in user.
-        user_id, _, user_realname = self.user_mgr.retrieve_user(username)
-        if user_id is None:
-            self.log_error('Unknown user ID')
-            raise RedirectException(LOGIN_URL)
-
-        # Render from template.
-        html_file = os.path.join(self.root_dir, HTML_DIR, 'task_status.html')
-        my_template = Template(filename=html_file, module_directory=self.tempmod_dir)
-        return my_template.render(nav=self.create_navbar(True), product=PRODUCT_NAME, root_url=self.root_url, email=username, name=user_realname)
+        return self.render_simple_page('task_status.html')
 
     @statistics
     def profile(self):
