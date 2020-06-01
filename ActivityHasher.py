20c6e6712a13949ab5e71394b69eb0d132d5870c
Mike Simms <msimms01@gmail.com>
2020-06-01 12:45:17 -0400

    When hashing sensor data, truncate to int.

--- ActivityHasher.py
+++ ActivityHasher.py
@@ -65,10 +65,10 @@ class ActivityHasher(object):
                     for datum in self.activity[sensor_type]:
                         if sys.version_info[0] < 3:
                             time = str(int(datum.keys()[0])).encode('utf-8')
-                            value = self.floatToStr(float(datum.values()[0]))
+                            value = str(int(datum.values()[0]))
                         else:
                             time = str(int(list(datum.keys())[0])).encode('utf-8')
-                            value = self.floatToStr(float(list(datum.values())[0]))
+                            value = str(int(list(datum.values())[0]))
 
                         h.update(time)
                         h.update(value)
