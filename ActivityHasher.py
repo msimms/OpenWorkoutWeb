8aa73c6563b88a07d65788e1eecf4c4cef2f45a1
Mike Simms <msimms01@gmail.com>
2020-07-16 21:37:42 -0400

    :bug: Handle both ints and floats when hashing an activity.

--- ActivityHasher.py
+++ ActivityHasher.py
@@ -13,9 +13,13 @@ class ActivityHasher(object):
         self.activity = activity
         super(ActivityHasher, self).__init__()
 
-    def floatToStr(self, num):
+    def float_to_str(self, num):
         formatted_str = "{:.6f}".format(num)
         return formatted_str.encode('utf-8')
+
+    def str_num_to_str(self, str_num):
+        formatted_str = str(int(float(str_num)))
+        return formatted_str
 
     def hash(self):
         """Main analysis routine."""
@@ -33,9 +37,9 @@ class ActivityHasher(object):
             locations = self.activity[Keys.ACTIVITY_LOCATIONS_KEY]
             for location in locations:
                 date_time = str(int(location[Keys.LOCATION_TIME_KEY])).encode('utf-8')
-                latitude = self.floatToStr(location[Keys.LOCATION_LAT_KEY])
-                longitude = self.floatToStr(location[Keys.LOCATION_LON_KEY])
-                altitude = self.floatToStr(location[Keys.LOCATION_ALT_KEY])
+                latitude = self.float_to_str(location[Keys.LOCATION_LAT_KEY])
+                longitude = self.float_to_str(location[Keys.LOCATION_LON_KEY])
+                altitude = self.float_to_str(location[Keys.LOCATION_ALT_KEY])
 
                 h.update(date_time)
                 h.update(latitude)
@@ -53,9 +57,9 @@ class ActivityHasher(object):
                 if sensor_type == Keys.APP_ACCELEROMETER_KEY:
                     for datum in self.activity[sensor_type]:
                         time = str(datum[Keys.ACCELEROMETER_TIME_KEY]).encode('utf-8')
-                        x = self.floatToStr(datum[Keys.ACCELEROMETER_AXIS_NAME_X])
-                        y = self.floatToStr(datum[Keys.ACCELEROMETER_AXIS_NAME_Y])
-                        z = self.floatToStr(datum[Keys.ACCELEROMETER_AXIS_NAME_Z])
+                        x = self.float_to_str(datum[Keys.ACCELEROMETER_AXIS_NAME_X])
+                        y = self.float_to_str(datum[Keys.ACCELEROMETER_AXIS_NAME_Y])
+                        z = self.float_to_str(datum[Keys.ACCELEROMETER_AXIS_NAME_Z])
 
                         h.update(time)
                         h.update(x)
@@ -64,11 +68,11 @@ class ActivityHasher(object):
                 else:
                     for datum in self.activity[sensor_type]:
                         if sys.version_info[0] < 3:
-                            time = str(int(datum.keys()[0])).encode('utf-8')
-                            value = str(int(datum.values()[0]))
+                            time = self.str_num_to_str(datum.keys()[0])
+                            value = self.str_num_to_str(datum.values()[0])
                         else:
-                            time = str(int(list(datum.keys())[0])).encode('utf-8')
-                            value = str(int(list(datum.values())[0])).encode('utf-8')
+                            time = self.str_num_to_str(list(datum.keys())[0]).encode('utf-8')
+                            value = self.str_num_to_str(list(datum.values())[0]).encode('utf-8')
 
                         h.update(time)
                         h.update(value)
