2a89c80aa18ff495c3c792df58b75f62a328058b
Mike Simms <msimms01@gmail.com>
2021-03-29 20:56:38 -0400

    :art: Code cleanup and some extra safety checks.

--- LocationAnalyzer.py
+++ LocationAnalyzer.py
@@ -271,16 +271,16 @@ class LocationAnalyzer(SensorAnalyzer.SensorAnalyzer):
 
                     # Examine the lines between the peaks. Extract pertinant data, such as avg speed/pace and set it aside.
                     # This data is used later when generating the report.
-                    all_intervals = []
+                    filtered_interval_list = []
                     for peak in peak_list:
                         interval = self.examine_interval_peak(peak.left_trough.x, peak.right_trough.x)
                         if interval is not None:
-                            all_intervals.append(interval)
+                            filtered_interval_list.append(interval)
 
                     # Do a k-means analysis on the computed speed/pace blocks so we can get rid of any outliers.
                     significant_intervals = []
                     num_speed_blocks = len(self.speed_blocks)
-                    if num_speed_blocks > 1:
+                    if num_speed_blocks >= 2:
 
                         # Make the data two dimensional because this is needed for the k means algorithm.
                         x1 = np.array(self.speed_blocks)
@@ -299,11 +299,15 @@ class LocationAnalyzer(SensorAnalyzer.SensorAnalyzer):
                         distortions = []
                         for k in range(1, max_k):
                             kmeans_model = KMeans(n_clusters=k).fit(X)
-                            distortions.append(sum(np.min(cdist(X, kmeans_model.cluster_centers_, 'euclidean'), axis = 1)) / X.shape[0])
+                            distances = cdist(X, kmeans_model.cluster_centers_, 'euclidean')
+                            distances_sum = sum(np.min(distances, axis = 1))
+                            distortion = distances_sum / X.shape[0]
+                            distortions.append(distortion)
 
                             # Use the elbow method to find the best value for k.
-                            if len(distortions) > 1:
+                            if len(distortions) >= 2 and k >= 2:
                                 slope = (distortions[k-1] + distortions[k-2]) / 2
+
                                 if best_k == 0 or slope > steepest_slope:
                                     best_k = k
                                     best_labels = kmeans_model.labels_
@@ -313,7 +317,7 @@ class LocationAnalyzer(SensorAnalyzer.SensorAnalyzer):
                         interval_index = 0
                         for label in best_labels:
                             if label >= 1:
-                                significant_intervals.append(all_intervals[interval_index])
+                                significant_intervals.append(filtered_interval_list[interval_index])
                             interval_index = interval_index + 1
 
                     results[Keys.ACTIVITY_INTERVALS_KEY] = significant_intervals
